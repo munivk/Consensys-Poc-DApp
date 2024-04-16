cbd959ca05d002292204e6419c85da247eef7bc9
Muni V K <munivenkateshnitk@gmail.com>
2024-04-16 13:59:53 -0600

    NFT smart contract code is added

---
+++ smart_contracts/NFT-erc721.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract MuniNFT is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+    event MintEvent(address indexed receiver, uint256 tokenId, string uri);
+
+    constructor()
+        ERC721("MuniNFT", "MNFT")
+        Ownable(msg.sender)
+    {}
+
+    function safeMint(string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        emit MintEvent(msg.sender, tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
